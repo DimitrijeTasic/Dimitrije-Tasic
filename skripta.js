e6355e8214f13065433088fb01aba3ec0f99a24d
DimitrijeTasic <dimitrijetasic525@gmail.com>
2023-12-10 19:29:14 +0100

    napravio stranicu sa listom predmeta

--- skripta.js
+++ skripta.js
@@ -1,68 +1,48 @@
-let lista=
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> 
+let predmeti=
 [
     {
-        "tekst":"Veb programiranje",
-        "status":"Završeno"
+        "tekst": "veb-progrmiranje",
+        "status": "dodato"
     },
     {
-        "tekst":"Paradigme",
-        "status":"Dodato"
+        "tekst": "programske paradigme",
+        "status": "započeto"
     },
     {
-        "tekst":"Baze podataka",
-        "status":"Završeno"
+        "tekst": "baze podataka",
+        "status": "završreno"
     },
     {
-        "tekst":"Matematika",
-        "status":"Započeto"
+        "tekst": "matematika",
+        "status": "dodato"
     },
     {
-        "tekst":"Fizika",
-        "status":"Dodato"
+        "tekst": "fizika",
+        "status": "dodato"
     },
     {
-        "tekst":"Srpski jezik",
-        "status":"Završeno"
+        "tekst": "srpski jezik",
+        "status": "započeto"
     },
     {
-        "tekst":"Engleski jezik",
-        "status":"Dodato"
+        "tekst": "engleski jezik",
+        "status": "započeto"
     },
     {
-        "tekst":"Biologija",
-        "status":"Započeto"
+        "tekst": "biologija",
+        "status": "završreno"
     },
     {
-        "tekst":"Filozofija",
-        "status":"Započeto"
+        "tekst": "sociologija",
+        "status": "završreno"
     },
     {
-        "tekst":"Sociologija",
-        "status":"Dodato"
+        "tekst": "filozofija",
+        "status": "dodato"
     },
     {
-        "tekst":"Fizičko",
-        "status":"Započeto"
-    }
+        "tekst": "fizičko",
+        "status": "završreno"
+    }   
 ]
-function checkboks(checkbox){
-    targetClass = checkbox.getAttribute('data-target-class')
-    console.log(targetClass)
-    if (checkbox.checked == true){
-        $(targetClass).show()
-    }
-    else
-        $(targetClass).hide()
-}
-function renderpredmet(u)
-{
-    $card = $(`<div class='card col ${u["tekst"]}'></div>`)
-    $cardBody = $("<div class='card-body d-flex flex-column align-items-center gap-3'></div>")
-    $card.append($cardBody)
-    $cardBody.text(u['status'] + " " + u['prezime'])
-    return $card
-}
-function renderListu(target, data)
-{
-    data.map(u => $(target).append(renderpredmet(u)))
-}
